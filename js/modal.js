8ad9ca7cf16706e2552c65972dcfd26b59d83d12
Andrew E <45772398+DrewEvans@users.noreply.github.com>
2021-04-15 11:24:49 +0200

    refactor code and add comments

--- js/modal.js
+++ js/modal.js
@@ -43,17 +43,22 @@ signupForm.addEventListener('submit', e => {
 
   e.preventDefault();
 
+  //if data validation fails tests return false 
   if (!validatedUserData) {
     return false;
+    //if validation passes display submission successful msg
   } else {
 
-    const inputFields = document.querySelector('.input-container');
+    const inputFields = document.querySelector('.input-container'); //targets parent element of formData  
 
+    //add style to inputFields hidding the container
     inputFields.classList.add("form-validation");
+
+    //remove class from msgSuccess
     msgSuccess.classList.remove("submission-notification-default");
+    //add new class to msgSuccess
     msgSuccess.classList.add("submission-notification");
-
-
+    //change value of btn-submit
     btnSubmit.value = "Fermer";
 
     //close modal form after submission
@@ -62,7 +67,7 @@ signupForm.addEventListener('submit', e => {
       if (e.isTrusted) {
         //change css display to none
         modalbg.style.display = "none";
-        signupForm.submit();
+
       }
     });
   }
@@ -74,12 +79,9 @@ function validate(userData) {
   const namePattern = /^[a-zA-ZÀ-ÿ-. ]{2,}$/
   const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   const birthdatePattern = /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/
-  const quantityPattern = /^[0-9]{0,2}$/
+  const quantityPattern = /^[0-9]{2,2}$/
 
-  //form inputs by user 
-
-  //console.log(userData);
-
+  //user Data inputs from form submission
   const {
     first: firstName,
     last: lastName,
@@ -91,68 +93,52 @@ function validate(userData) {
     location1: location
   } = userData
 
-  const locations = userData.location;
-  //console.log(locations.value);
+  const locations = userData.location; //Returns value of user selected location
 
-  let errors = [];
+  let errors = []; //empty array to log if any validations fail
 
+  let locationChecked = false; //default location input
+
+  //loop over all location radio buttons 
   for (let i = 0; i < locations.length; i++) {
+    //record if any have been checked by user 
     const isChecked = locations[i].checked;
-    //console.log(isChecked)
-
-    if (!isChecked) {
-      removeError(location);
+    //if one is true update 
+    //locationChecked and break loop
+    if (isChecked) {
+      locationChecked = true;
       break;
-    } else {
-      showError(location, 'Vous devez choisir une option.')
-
     }
   }
 
-  if (namePattern.test(firstName.value)) {
-    removeError(firstName);
-  } else {
-    showError(firstName, 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.');
-    errors.push('error');
-  }
+  //Validation logic for required fields from user
+  //if test passes display default
+  //else target the the html element
+  // display error msg and push an error
+  // to var errors 
 
-  if (namePattern.test(lastName.value)) {
-    removeError(lastName);
-  } else {
-    showError(lastName, 'Veuillez entrer 2 caractères ou plus pour le champ du nom.');
-    errors.push('error');
-  }
+  //is location checked test  
+  locationChecked ? removeError(location1) : (showError(location1, "Vous devez choisir une option."), errors.push('error'))
 
-  if (emailPattern.test(email.value)) {
-    removeError(email);
-  } else {
-    showError(email, "Adresse e-mail invalide");
-    errors.push('error');
-  }
+  //test firstName input agaisnt validation regex
+  namePattern.test(firstName.value) ? removeError(firstName) : (showError(firstName, 'Veuillez entrer 2 caractères ou plus pour le champ du nom.'), errors.push('error'))
 
-  if (birthdatePattern.test(birthdate.value)) {
-    removeError(birthdate);
-  } else {
-    showError(birthdate, "Vous devez entrer votre date de naissance.");
-    errors.push('error');
-  }
+  //test lastName input agaisnt validation regex
+  namePattern.test(lastName.value) ? removeError(lastName) : (showError(lastName, 'Veuillez entrer 2 caractères ou plus pour le champ du nom.'), errors.push('error'))
 
-  if (quantityPattern.test(numOfTournaments.value)) {
-    removeError(numOfTournaments);
-  } else {
-    showError(numOfTournaments, "Entrez un numéro valide à 2 chiffres.")
-    errors.push('error');
-  }
+  //test email input agaisnt validation regex
+  emailPattern.test(email.value) ? removeError(email) : (showError(email, "Adresse e-mail invalide"), errors.push('error'))
 
-  if (terms.checked) {
-    removeError(terms)
-  } else {
-    showError(terms, "Vous devez vérifier que vous acceptez les termes et conditions.")
-    errors.push('error');
-  }
+  //test birthdate input agaisnt validation regex
+  birthdatePattern.test(birthdate.value) ? removeError(birthdate) : (showError(birthdate, "Vous devez entrer votre date de naissance."), errors.push('error'))
 
-  console.log(`Errors: ${errors.length}`);
+  //test firstName input agaisnt validation regex
+  quantityPattern.test(numOfTournaments.value) ? removeError(numOfTournaments) : (showError(numOfTournaments, "Entrez un numéro valide à 2 chiffres."), errors.push('error'))
 
+  //test terms checkbox to see if checked
+  terms.checked ? removeError(terms) : (showError(terms, "Vous devez vérifier que vous acceptez les termes et conditions."), errors.push('error'))
+
+  //Submitted userData
   console.log(
     firstName.value,
     lastName.value,
@@ -161,17 +147,24 @@ function validate(userData) {
     numOfTournaments.value,
     locations.value,
     terms.checked,
-    notifications.checked);
+    notifications.checked
+  );
 
+  //number of errors logged on submit  
+  console.log(`Errors: ${errors.length}`);
+
+  //if any errors recored return false
+  //else function returns true 
   if (errors.length > 0) {
     return false;
   }
   return true;
 }
 
+//if validation is incorect show user error msg
 function showError(input, message) {
-  //target div.formData from data input
-  const formControl = input.parentElement;
+
+  const formControl = input.parentElement; //parent formData div from userData input
 
   //set attribute to data-error and pass error msg
   formControl.setAttribute('data-error', message);
@@ -180,11 +173,12 @@ function showError(input, message) {
   formControl.setAttribute('data-error-visible', 'true');
 }
 
+//if validation passes display the default
 function removeError(input) {
-  //target div.formData from data input
-  const formControl = input.parentElement;
 
-  //remove attributes from html and display default design
+  const formControl = input.parentElement; //parent formData div from userData input
+
+  //remove attributes and display default style
   formControl.removeAttribute('data-error-visible', '');
   formControl.removeAttribute('data-error', '');
 }
